9bdd50477bc8901d27de8677c19457b70f0e8650
Animesh Gupta <animeshh04@gmail.com>
2019-08-30 00:41:28 -0400

    add unitconverter
    add unitconverter files

---
+++ Constants.java
@@ -0,0 +1,8 @@
+package com.unitconverter;
+
+public class Constants {
+
+    static final String OUTPUT_CORRECT = "correct";
+    static final String OUTPUT_INCORRECT = "incorrect";
+    static final String OUTPUT_INVALID = "invalid";
+}
